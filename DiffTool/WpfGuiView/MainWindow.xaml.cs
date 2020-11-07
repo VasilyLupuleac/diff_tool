eed614a5b7719abf8394755533775fb2ba45dd8b
Василий Лупуляк <v_lupuleac@mail.ru>
2020-11-07 09:51:20 +0300

    Add dummy GUI view project

---
+++ DiffTool/WpfGuiView/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGuiView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog();
+
+            var result = dlg.ShowDialog();
+
+            if (result == true)
+                file1.Text = dlg.FileName;
+        }
+        private void Button2Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog();
+
+            var result = dlg.ShowDialog();
+
+            if (result == true)
+                file2.Text = dlg.FileName;
+        }
+    }
+}
